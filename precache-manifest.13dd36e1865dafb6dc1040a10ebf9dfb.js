5cadfe38f4aa6d26bd64d2640c74def18b688f41
User <den.dmytruk@gmail.com>
2020-03-29 01:40:48 +0200

    Updates

---
+++ precache-manifest.13dd36e1865dafb6dc1040a10ebf9dfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa668626a82881f001d035ed258f169b",
+    "url": "/your-todo-list/index.html"
+  },
+  {
+    "revision": "5993c851e80ce414b2d9",
+    "url": "/your-todo-list/static/css/main.6cddefa7.chunk.css"
+  },
+  {
+    "revision": "69dfbc4927e73c3623a7",
+    "url": "/your-todo-list/static/js/2.d485f0c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/your-todo-list/static/js/2.d485f0c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5993c851e80ce414b2d9",
+    "url": "/your-todo-list/static/js/main.2fffe83d.chunk.js"
+  },
+  {
+    "revision": "97a8c68e680525e00806",
+    "url": "/your-todo-list/static/js/runtime-main.de261339.js"
+  }
+]);
